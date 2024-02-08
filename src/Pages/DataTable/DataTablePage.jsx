1ddd77309b3fb3d384777a7cb4b17cdd86e2b256
HelyssonBN3040 <helyssoncavalcante.com@gmail.com>
2024-02-08 12:48:56 -0300

    sidebar e datatable

--- src/Pages/DataTable/DataTablePage.jsx
+++ src/Pages/DataTable/DataTablePage.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../DataTable/DataTable.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import DataTable from 'react-data-table-component'
 import SideBar from '../../components/sidebar/SideBar.jsx'
 
 const DataTablePage = () => {
+
+  const columns = [
+    {
+      name: 'Nome',
+      selector: row => row.name,
+      sortable: true
+    },
+    {
+      status: "Status",
+      selector: row => row.status,
+      sortable: true
+    }
+  ]
+  const data =
+    [
+      {
+        id: 1,
+        name: "Helysson Cavalcante",
+      },
+      {
+        id: 2,
+        name: "Mabyle Jeandressa",
+      },
+      {
+        id: 3,
+        name: "João Victor Almeida",
+      },
+      {
+        id: 4,
+        name: "Jose Silva Souza",
+      }
+    ]
+
+  const [records, setRecords] = useState(data);
+  function handleFilter(event) {
+    const newData = data.filter(row => {
+      return row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setRecords(newData)
+  }
+
   return (
-    <div>
-        <SideBar/>
-        <h1>data</h1>
+    <div className="d-flex">
+      <div className="col-auto">
+        <SideBar />
+      </div>
+      <div className='ContainerDataTable'>
+        <div className="text-end mt-2"><input type="text" onChange={handleFilter}/></div>
+        <DataTable
+          columns={columns}
+          data={data}
+          selectableRows
+          fixedHeader
+          pagination
+        ></DataTable>
+      </div>
     </div>
   )
 }
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import "../sidebar/SideBar.css"
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
+    <div className='sidebar d-flex flex-column justify-content-between bg-dark text-white p-4 vh-100'>
+      <div>
+        <a href="" className='d-flex align-items-center text-white text-decoration-none '>
+          <i className='bi bi-bootstrap fs-5 me-2'></i>
+          <span className='fs-4'>SideBar</span>
+        </a>
+        <hr className='text-secundary mt-2' />
+        <ul className='nav nav-pills flex-column p-0 m-0'>
+          <li className='nav-item p-1'>
+            <a href="" className='nav-link text-white'>
+              <i className='bi bi-speedometer me-2'></i>
+              <span className='fs-5'>Dashboard</span>
+            </a>
+          </li>
+          <li className='nav-item p-1'>
+            <a href="" className='nav-link text-white'>
+              <i className='bi bi-table me-2'></i>
+              <span className='fs-5'>Mabyle</span>
+            </a>
+          </li>
+          <li className='nav-item p-1'>
+            <a href="" className='nav-link text-white'>
+              <i className='bi bi-people me-2'></i>
+              <span className='fs-5'>Order</span>
+            </a>
+          </li>
+          <li className='nav-item p-1'>
+            <a href="" className='nav-link text-white'>
+              <i className='bi bi-speedometer me-2'></i>
+              <span className='fs-5'>Dashboard</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <hr className='text-secondary' />
+        <i className='bi bi-person fs-5'></i>
+        <span>Helysson</span>
+      </div>
+    </div>
+    )
 }
 
 export default SideBar
